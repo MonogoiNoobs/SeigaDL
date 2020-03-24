1ff696e4e38c3ef6c06d6959823d67f988110422
MonogoiNoobs <62567343+MonogoiNoobs@users.noreply.github.com>
2020-03-25 03:17:50 +0900

    kaigyou ko-do henkou

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SeigaDL
-FirefoxにはあったけどChromeにはなかった静画ワンクリックダウンローダー
 
-# 使い方(新Edge)
-1. Clone or downloadからZIPをダウンロードして解凍する
+Firefox にはあったけど Chrome にはなかった静画ワンクリックダウンローダー
+
+# 使い方(新 Edge)
+
+1. Clone or download から ZIP をダウンロードして解凍する
 1. edge://extensions/の開発者モードをオンにする
 1. 「展開して読み込み」から解凍したフォルダを選ぶ
--- background.js
+++ background.js
@@ -13,7 +13,7 @@ const replacementMap = new Map([
   [":", "："],
   ["?", "？"],
   ["*", "＊"],
-  ['"', '”']
+  ['"', "”"]
 ]);
 
 const escapeForbiddenCharactersFrom = origin => {
@@ -24,10 +24,10 @@ const escapeForbiddenCharactersFrom = origin => {
     result = result.replace(regex, escaped);
   }
   return result;
-}
+};
 
 const fetchAs = async (mime, url) => {
-  const response = await fetch(url, {credentials: "include"});
+  const response = await fetch(url, { credentials: "include" });
   const text = await response.text();
   return new DOMParser().parseFromString(text, mime);
 };
@@ -41,24 +41,33 @@ const downloadFrom = url => {
 
 const determiningCallback = (downloadItem, suggest) => {
   suggest({
-    filename: `SeigaDL/${escapeForbiddenCharactersFrom(data.nickname)}_${data.userId}/${escapeForbiddenCharactersFrom(data.title)}_${downloadItem.filename}`
+    filename: `SeigaDL/${escapeForbiddenCharactersFrom(data.nickname)}_${
+      data.userId
+    }/${escapeForbiddenCharactersFrom(data.title)}_${downloadItem.filename}`
   });
   chrome.downloads.onDeterminingFilename.removeListener(determiningCallback);
 };
 
 chrome.runtime.onMessage.addListener(message => {
-  fetchAs("text/xml", `https://seiga.nicovideo.jp/api/illust/info?id=${message.id}`)
-    .then(xml => {
-      data.userId = xml.querySelector("user_id").textContent;
-      data.title = xml.querySelector("title").textContent;
-      fetchAs("text/xml", `https://seiga.nicovideo.jp/api/user/info?id=${data.userId}`)
-        .then(userInfoXml => {
-          data.nickname = userInfoXml.querySelector("nickname").textContent;
-        });
+  fetchAs(
+    "text/xml",
+    `https://seiga.nicovideo.jp/api/illust/info?id=${message.id}`
+  ).then(xml => {
+    data.userId = xml.querySelector("user_id").textContent;
+    data.title = xml.querySelector("title").textContent;
+    fetchAs(
+      "text/xml",
+      `https://seiga.nicovideo.jp/api/user/info?id=${data.userId}`
+    ).then(userInfoXml => {
+      data.nickname = userInfoXml.querySelector("nickname").textContent;
     });
-  fetchAs("text/html", message.href)
-    .then(v => {
-      chrome.downloads.onDeterminingFilename.addListener(determiningCallback);
-      downloadFrom(`https://lohas.nicoseiga.jp/${v.querySelector("#content .illust_view_big").dataset.src}`);
-    });
+  });
+  fetchAs("text/html", message.href).then(v => {
+    chrome.downloads.onDeterminingFilename.addListener(determiningCallback);
+    downloadFrom(
+      `https://lohas.nicoseiga.jp/${
+        v.querySelector("#content .illust_view_big").dataset.src
+      }`
+    );
+  });
 });
